8ba0e0a385c5b19572cfb01b9b5c010817ad17aa
Andrés Carvajal <d.carvajal_13@hotmail.com>
2020-11-26 00:06:36 -0500

    [ADD] Test Suit Add of the User

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,23 @@
+module.exports = {
+    user: {
+      _id: "507f191e810c19729de860ea",
+      name: "Marluan",
+      userName: "Marluan03",
+      password: "mystrongPassword"
+    },
+    users: [
+      {
+        _id: "507f191e810c19729de860ea",
+        name: "Marluan",
+        userName: "Marluan03",
+        password: "mystrongPassword"
+      },
+      {
+        _id: "507f191e810c19729de860eb",
+        name: "Erick",
+        userName: "Erick_34",
+        password: "mystrongPassword"
+      }
+    ]
+  };
+  
